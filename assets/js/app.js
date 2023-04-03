5d28da1ba9098bbdbac45e50fc3ed0ebe02e42e7
Codesleep <125808990+codesleeps@users.noreply.github.com>
2023-04-03 15:49:30 +0100

    added current day month and year

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Add the current day month year to top of page
+$('#currentDay').text(moment().format('dddd, MMMM 2023'));
+
+//Make an array for work hours 9am - 5pm
+const workHours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+// for loop
+for(let i = 0; i < workHours.length; i++){
+  let hours = workHours[i];
+  let hoursString = moment(hours, 'hh A').format('hh A');
+  workHours[i] = hoursString;
+}
